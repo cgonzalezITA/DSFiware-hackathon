a4e9f38b7190e62fc5889f93f43f3f0a76059253
cgonzalez <cgonzalez@V23040>
2024-12-05 01:42:21 +0100

    Changes to update phase01.step02

--- scripts/quickInstall/dsQuickinstall-phase01.step02.sh
+++ scripts/quickInstall/dsQuickinstall-phase01.step02.sh
@@ -40,6 +40,7 @@ hFileCommand $ARTIFACT_NAME -v -y -b restart
 readAnswer "On the next screen wait till all the artifacts are properly deployed (1/1) for all; then press Ctrl+C and the process will continue. Even once available, the initialization of the apisix-data-plane can take several seconds, so be patient" "" 20 false
 kGet -w -v -n $NAMESPACE
 
+DNS_CONSUMER="fiwaredsc-consumer.local"
 CMD="curl -s -o /dev/null -w \"%{http_code}\" -k https://$DNS_CONSUMER"
 echo "# Running CMD=$CMD"
 RC=$($CMD)
