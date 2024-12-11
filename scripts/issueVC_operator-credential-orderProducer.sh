cabc0fd898fe325d8b423e28c74abc8da001555a
cgonzalez <cgonzalez@V23040>
2024-12-11 18:19:33 +0100

    Changes to update phase03.step04

---
+++ scripts/issueVC_operator-credential-orderProducer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script based on steps described on https://github.com/FIWARE/data-space-connector/blob/main/doc/deployment-integration/local-deployment/LOCAL.MD
+############################
+## Variable Initialization #
+############################
+SCRIPTNAME=$BASH_SOURCE
+if [ "$0" == "$BASH_SOURCE" ]; then CALLMODE="executed"; else CALLMODE="sourced"; fi
+BASEDIR=$(dirname "$SCRIPTNAME")
+
+VERBOSE=true
+STOP=false
+#############################
+## Functions               ##
+#############################
+
+URL_VCISSUER=https://fiwaredsc-consumer.local/realms/consumerRealm
+# https://fiwaredsc-consumer.local/realms/consumerRealm/account/oid4vci to retrieve the equivalent from a browser using a VCWallet
+ADMIN_CLI=admin-cli
+USER_01=op-user
+USER_01_PASSWORD=test
+CREDENTIAL_TYPE=operator-credential
+
+eval $BASEDIR/_issueVC.sh --vcIssuer $URL_VCISSUER --user $USER_01 --password $USER_01_PASSWORD --credentialType $CREDENTIAL_TYPE $@
