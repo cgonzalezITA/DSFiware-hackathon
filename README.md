9909143d80ad53d6e8483a6f64d2bd4bdacf9874
cgonzalez <cgonzalez@V23040>
2024-12-18 10:25:26 +0100

    Changes to update .

--- README.md
+++ README.md
@@ -92,7 +92,7 @@ This phase will show the actions to register the participants in the dataspace a
 
 ## Quick deployment from scratch
 To speed up the deployment, this github contains a [folder with script files (./scripts/quickinstall)](./scripts/quickInstall/) to perform the following actions  
-**NOTE**: _although these scripts have been tested in an `Ubuntu 20.04.6 LTS`, they may contain steps that require manual actions (such as editting a file with sudo permissions), so in case of failure, please review the logs and perform the steps manually (copying, pasting) for a better understanding of the whole process)_
+**NOTE**: _although these scripts have been tested in an `Ubuntu 20.04.6 LTS`, they may contain steps that require manual actions (such as editting a file with sudo permissions), althought they have been tried to be automated, so in case of failure, please review the logs and perform these steps manually (copying, pasting) for a better understanding of the whole process)_
 1. In case you do not have a kubernetes cluster on hand, the [./scripts/quickInstall/installMicrok8s.sh](./scripts/quickInstall/installMicrok8s.sh)  contains the steps to quickly deploy a microK8s cluster on one node.
     ```shell
     . scripts/quickInstall/installMicrok8s.sh
@@ -111,4 +111,28 @@ To speed up the deployment, this github contains a [folder with script files (./
     . scripts/quickInstall/dsQuickinstall-phase01.step01.sh
     . scripts/quickInstall/dsQuickinstall-phase01.step02.sh
     ...
+    . scripts/quickInstall/dsQuickinstall-phase05.step04.sh
     ```
+These quickInstall scripts may take several minutes to finalize as the whole infrastructure has to be updated and some components have to be downloaded from the internet. They are split in three phases:
+- Deployment
+- Configuration
+- Verification
+
+The `dsQuickinstall-phase05.step04.sh` deploys the complete data space infrastructure as shown in the following image. Among the multiple logs printed out at the console, the final ones -the verification of the correct deployment- should look something similar to:
+```shell
+ . DSFiware-hackathon/scripts/quickInstall/dsQuickinstall-phase05.step04.sh 
+    ...
+    # Verification
+    # First of all, a VC is issued to the user with ORDERCONSUMER role
+    DATA_SERVICE_ACCESS_TOKEN=eyJhbGciOi...H0Q
+    # Now, this VC is used to retrieve the JWT validated to access the service
+    DATA_SERVICE_ACCESS_TOKEN=eyJhbGciO...xWw
+    # Access to the service (with both authentication and authorization enabled):
+
+    RC=200
+    (timeout=5s)"curl -s -k https://fiwaredsc-provider.local/services/hackathon-service/ngsi-ld/v1/entities?type=Order     --header "Accept: application/json"     --header "Authorization: Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImpWX3YxYmsxMDRFWFVyRG5aZFBBYXhRNWNXZ1R5UUREdXpuWlAxNEJncjAiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOlsiaGFja2F0aG9uLXNlcnZpY2UiXSwiY2xpZW50X2lkIjoiZGlkOmtleTp6RG5hZVVuWkhkUFJxQTViR0J0aGtNSnB1RVNxRTlwaEZNMWpnU3A2ZmRzUFZaZ0REIiwiZXhwIjoxNzM0NTE1MDQ5LCJpc3MiOiJkaWQ6a2V5OnpEbmFlVW5aSGRQUnFBNWJHQnRoa01KcHVFU3FFOXBoRk0xamdTcDZmZHNQVlpnREQiLCJraWQiOiJqVl92MWJrMTA0RVhVckRuWmRQQWF4UTVjV2dUeVFERHV6blpQMTRCZ3IwIiwic3ViIjoiIiwidmVyaWZpYWJsZUNyZWRlbnRpYWwiOnsiQGNvbnRleHQiOlsiaHR0cHM6Ly93d3cudzMub3JnLzIwMTgvY3JlZGVudGlhbHMvdjEiLCJodHRwczovL3d3dy53My5vcmcvbnMvY3JlZGVudGlhbHMvdjEiXSwiY3JlZGVudGlhbFN1YmplY3QiOnsiZW1haWwiOiJvcmRlcmNvbnN1bWVydXNlckBjb25zdW1lci5vcmciLCJmaXJzdE5hbWUiOiJPcmRlckNvbnN1bWVyIiwibGFzdE5hbWUiOiJVc2VyIiwicm9sZXMiOlt7Im5hbWVzIjpbIk9SREVSX0NPTlNVTUVSIl0sInRhcmdldCI6ImRpZDprZXk6ekRuYWVhUUR0UGFWNTkyUzl2b1pxTHVlbmJDQW5qeDFleTR0UThhMzZVMnNyblY2aCJ9XX0sImlkIjoidXJuOnV1aWQ6ZGE2MzA1YzEtYjBjOS00ZTliLTlhYjUtNDZkY2U1Zjg1MDRlIiwiaXNzdWFuY2VEYXRlIjoiMjAyNC0xMi0xOFQwOToxMzo1N1oiLCJpc3N1ZXIiOiJkaWQ6a2V5OnpEbmFlYVFEdFBhVjU5MlM5dm9acUx1ZW5iQ0FuangxZXk0dFE4YTM2VTJzcm5WNmgiLCJ0eXBlIjpbIk9wZXJhdG9yQ3JlZGVudGlhbCJdfX0.sLsR8ioqQHBg3QVUyceSkqfzJt4wgjn8_tAptcXdjesWQwLD4BE7oF6ZvJNyZTlvEU5aUQm_PDig1D1xFozEIKt3RyRPfLoym0irwb8bLxHcGv9StvwvQDRNUQg1nwjE_FUosvk9jI0c6UKdYrFTRo0rbhNBB_83JjVstGDj9KhZ2Fp1t0ksEOhjEoBPpWxClDr1JgM2srmVw4KPF7vfJgQh2au7C8fPq1nmXypagvCykJJN5ReK6NakV914KNOnWl_csDIhjWK2_vzTKMdPaCA-uf2FOqvFQj-dqhJ5M5oBUhtOCAigR9S67fDCpuXjJP5OxB9r21vk_SeOPmsxWw"" 
+    returns a valid json. It has worked! Congrats!.
+```
+
+<p style="text-align:center;font-style:italic;font-size: 75%"><img src="./assets/images/Fiware-DataSpaceGlobalArch-phase05.png"><br/>
+Architecture after the provider components deployment is completed</p>
---
+++ scripts/quickInstall/dsQuickinstall-phase05.step04.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+SCRIPTNAME=$BASH_SOURCE
+if [ "$0" == "$BASH_SOURCE" ]; then CALLMODE="executed"; else CALLMODE="sourced"; fi
+BASEDIR=$(dirname "$SCRIPTNAME")
+VERBOSE=true
+SUBMODULE=""
+
+. $BASEDIR/_base.sh
+SCRIPTNAME=$BASH_SOURCE
+
+unset DID1
+#---------------------------------------------- main program ------------------------
+echo "##############################"
+echo "# Install the DSFiware-hackathon github repository"
+DSFIWAREHOL_GH_HTTPS="https://github.com/cgonzalezITA/DSFiware-hackathon.git"
+DSFIWAREHOL_TAG="phase05.step03" # replace by phase05.step04
+STEP_DESCRIPTION="Addition of the authorization checking to the service route"
+DSFIWAREHOL_FOLDER="DSFiware-hackathon-$DSFIWAREHOL_TAG"
+PUBLIC_IP=$(hostname -I | awk '{print $1}')
+DNS_PROVIDER="fiwaredsc-provider.local"
+REPLY='y'
+
+if [[ -d $DSFIWAREHOL_FOLDER ]]; then
+    REPLY=$(readAnswer  "Folder $DSFIWAREHOL_FOLDER already exists. Do you want to delete it to reinstall the DSFiware-hackathon git? (Y|n*)" \
+        'n');
+    [[ $REPLY == 'y' ]] && sudo rm -r $DSFIWAREHOL_FOLDER;
+fi
+if [[ $REPLY == 'y' ]]; then
+    CMD="git clone $DSFIWAREHOL_GH_HTTPS $DSFIWAREHOL_FOLDER -b $DSFIWAREHOL_TAG"
+    echo "Running CMD=$CMD"
+    bash -c "$CMD"
+fi
+
+echo "# $DSFIWAREHOL_TAG-$STEP_DESCRIPTION"
+
+DSFIWAREHOL_FOLDER="DSFiware-hackathon" # TODO DELETE
+echo "# Jumping into the hackathon folder ($DSFIWAREHOL_FOLDER)"
+cd $DSFIWAREHOL_FOLDER
+echo "Now at $(pwd) folder"
+
+echo "# Upgrade the apisix configuration"
+kRemoveRestart -n apisix data-plane -v -y
+hFileCommand apisix upgrade -b -v -y
+
+export NAMESPACE="consumer"
+echo "# Removes previously existing namespace $NAMESPACE"
+hFileCommand $NAMESPACE down -y
+kRemoveRestart ns $NAMESPACE -y
+echo "# Deployment of the $NAMESPACE helm"
+hFileCommand $NAMESPACE restart -v -y -b
+
+export NAMESPACE="provider"
+echo "# Deployment of the provider common"
+hFileCommand provider/common restart -v -y -b
+echo "# Deployment of the provider authentication"
+hFileCommand provider/authentication restart -v -y -b
+echo "# Deployment of the provider authorization"
+hFileCommand provider/authorization restart -v -y -b
+
+echo "# Deployment of the provider service"
+hFileCommand provider/service restart -v -y -b
+
+
+
+echo "# Registration of the new apisix routes"
+. scripts/manageAPI6Routes.sh insert -r ROUTE_WELLKNOWN_OIDC_fiwaredsc_vcverifier_local
+. scripts/manageAPI6Routes.sh insert -r ROUTE_WELLKNOWN_JWKS_fiwaredsc_vcverifier_local
+. scripts/manageAPI6Routes.sh insert -r ROUTE_WELLKNOWN_OIDC_Service_fiwaredsc_vcverifier_local
+# . scripts/manageAPI6Routes.sh insert -r ROUTE_PROVIDER_SERVICE_fiwaredsc_provider_local_0auth
+. scripts/manageAPI6Routes.sh insert -r ROUTE_PROVIDER_SERVICE_fiwaredsc_provider_local_2auth
+. scripts/manageAPI6Routes.sh insert -r ROUTE_PROVIDER_fiwaredsc_provider_local_dataSpaceConfiguration
+
+echo "# Registers the fiwaredsc-provider.local at the /etc/hosts file to map the DNS with the IP address"
+LINE="$PUBLIC_IP  $DNS_PROVIDER"
+echo "# Map the local DNS at your hosts file"
+MSG="# To use the DNS $DNS_PROVIDER at the host, it is required to add a new line \"$LINE\" to the '/etc/hosts' file.\n\
+Do you want to insert it automatically?";
+if [ $(readAnswer "$MSG (y|n*)" 'n') == 'y' ]; then
+    sudo cat <<EOF >> /etc/hosts
+$LINE
+EOF
+    if [[ "$?" -ne 0 ]]; then
+        readAnswer "An error has happened. This operation requires sudo permission. Do it manually on another terminal and press any key to continue" \
+            "" 120 false false
+    fi
+fi
+readAnswer "To access it from a windows browser, add the same line into the 'C:\Windows\System32\drivers\etc\hosts' file\n\
+    Press a key to continue" "" 10 false false
+
+
+
+# Waits for the deployment
+wait4PodsDeploymentCompleted apisix 20
+wait4PodsDeploymentCompleted service 20 "Note that the init-data pod finishes when it is marked as '0/1 Completed'. Please, be patient"
+wait4PodsDeploymentCompleted provider 20
+wait4PodsDeploymentCompleted consumer 20
+
+
+echo "# Verification"
+unset VERIFIABLE_CREDENTIAL
+unset DATA_SERVICE_ACCESS_TOKEN
+echo "# First of all, a VC is issued to the user with ORDERCONSUMER role"
+export VERIFIABLE_CREDENTIAL=$(./scripts/issueVC_operator-credential-orderConsumer.sh -v)
+echo "DATA_SERVICE_ACCESS_TOKEN=$VERIFIABLE_CREDENTIAL"
+echo "# Now, this VC is used to retrieve the JWT validated to access the service"
+export DATA_SERVICE_ACCESS_TOKEN=$(. scripts/generateAccessTokenFromVC.sh $VERIFIABLE_CREDENTIAL -v)
+echo "DATA_SERVICE_ACCESS_TOKEN=$DATA_SERVICE_ACCESS_TOKEN"
+echo "# Access to the service (with both authentication and authorization enabled):"
+export DNS="https://fiwaredsc-provider.local/services/hackathon-service/ngsi-ld/v1/entities?type=Order"
+export CMD="curl -s -o /dev/null -w \"%{http_code}\" -k $DNS \
+    --header \"Accept: application/json\" \
+    --header \"Authorization: Bearer ${DATA_SERVICE_ACCESS_TOKEN}\""
+export RC=$(bash -c "$CMD")
+echo -e "\nRC=$RC"
+if test "$RC" -eq 200; then
+    export CMD="curl -s -k $DNS \
+    --header \"Accept: application/json\" \
+    --header \"Authorization: Bearer ${DATA_SERVICE_ACCESS_TOKEN}\""
+    JSON=$(bash -c "$CMD")
+    echo "$JSON" | jq empty
+    if [ $? -eq 0 ]; then
+        readAnswer "\"$CMD\" returns a valid json. It has worked! Congrats!." "" 5 false
+    else
+        readAnswer "\"$CMD\" has failed. Review the logs and the value file used by helm for some clues" "" 15 false
+    fi
+    
+else
+    readAnswer "\"$CMD\" has failed (RC=$RC). Review the logs and the value file used by helm for some clues" "" 15 false
+fi
+
+cd ..
